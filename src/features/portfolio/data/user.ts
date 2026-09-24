5617af582392a9226df4b0f21472ee0b9b069305
Yash-Tibadiya <tibadiyayash@gmail.com>
2026-09-24 17:19:16 +0530

    Update user email in portfolio data to new address for accuracy.

--- src/features/portfolio/data/user.ts
+++ src/features/portfolio/data/user.ts
@@ -16,7 +16,7 @@ export const USER: User = {
   address: "Surat, India",
   // base64 encoded
   phoneNumberB64: "KzkxODc5OTQ0MjEwMQ==", // +91 87994 42101
-  emailB64: "dGliYWRpeWF5YXNoQGdtYWlsLmNvbQ==", // tibadiyayash@gmail.com
+  emailB64: "aGVsbG9AeWFzaDE0LmNvbQ==", // hello@yash14.com
   website: "https://yash14.com",
   jobTitle: "Full Stack Developer",
   jobs: [
